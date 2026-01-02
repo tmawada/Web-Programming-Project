3a204a51105c23a89e25fe2ee3ba45a25e12060f
unknown <tmawadax@gmail.com>
2026-01-02 15:41:27 +0700

    Archex V1.5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('local')) {
+        if (app()->environment('production')) {
             Artisan::call('db:seed', ['--force' => true]);
         }
 
